8d6d11ef0df6b0960d710c3b68361004914c1419
scumblrtest2 <sbehrens+scumblrtest2@gmail.com>
2017-02-23 13:42:13 -0800

    Update test.rb

--- test.rb
+++ test.rb
@@ -1,12 +1,14 @@
 puts 'ruby whatever'
 has some words
 that may
+7fbdb9a3-3b8f-459a-886c-a38d6d069ce9
 be interesting
 foo
 blah
 bhlah
 askljsaf
 salkjfdsa
+a6365bfb-cb41-4f2c-9c5f-2e904d0abaa8
 bar
 lkjsaf
 jksaf
